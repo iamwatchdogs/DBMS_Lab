83c6379f05e2849bc89c312fd875647f5c05cb48
iamwatchdogs <shamith301102@gmail.com>
2022-09-28 01:59:06 +0530

    Finished ../Q2_Sol.sql

---
+++ DML_Commands/Challenge_Yourself/Q2_Sol.sql
@@ -0,0 +1,8 @@
+INSERT INTO Customers (customer_id,first_name,last_name,email) VALUES (101,'Sam','N','sam@gmail.com');
+INSERT INTO Customers (customer_id,first_name,last_name,email) VALUES (102,'Ram','D','DeeRam@gmail.com');
+INSERT INTO Customers (customer_id,first_name,last_name,email) VALUES (103,'Pavan','K','pavan459@gmail.com');
+INSERT INTO Customers (customer_id,first_name,last_name,email) VALUES (104,'Sita','P','sita2001@gmail.com');
+INSERT INTO Customers (customer_id,first_name,last_name,email) VALUES (105,'Gita','V','artistno1@gmail.com');
+INSERT INTO Customers (customer_id,first_name,last_name,email) VALUES (106,'Sai Kumar','B','Kumarrocks@hotmail.com');
+INSERT INTO Customers (customer_id,first_name,last_name,email) VALUES (107,'Sumanth','P','single4life@gmail.com');
+SELECT COUNT(*) FROM Customers;
