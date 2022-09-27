8d40c6ff087d4e8a37bd4610c763685537994bf5
iamwatchdogs <shamith301102@gmail.com>
2022-09-28 01:30:29 +0530

    Finished ../Q1_Sol.sql

---
+++ DML_Commands/Skill_Builder_Oracle/Q1_Sol.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee_details VALUES (101,'Jane Austin',92586);
+INSERT INTO employee_details VALUES (102,'Chris Dope',74857);
+INSERT INTO employee_details VALUES (103,'Wilster Grill',96582);
+INSERT INTO employee_details VALUES (104,'Rose Kinsley',95867);
+INSERT INTO employee_details VALUES (105,'Tine Dsouza',75928);
+INSERT INTO employee_details VALUES (106,'Louis Fantom',75896);
+SELECT * FROM employee_details;
