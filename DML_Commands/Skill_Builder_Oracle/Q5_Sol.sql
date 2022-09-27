3a03b8b40948508d9e62a62d4c42799a52c2034c
iamwatchdogs <shamith301102@gmail.com>
2022-09-28 01:33:42 +0530

    Finished ../Q5_Sol.sql

---
+++ DML_Commands/Skill_Builder_Oracle/Q5_Sol.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer_details (customer_id,location) VALUES (1,'Florida');
+INSERT INTO customer_details (customer_id,location) VALUES (2,'California');
+INSERT INTO customer_details (customer_id,location) VALUES (3,'Washington');
+SELECT * FROM customer_details;
