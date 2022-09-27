9ce6cedcdfcfb3ee90724d784d69c64dc91a39f6
iamwatchdogs <shamith301102@gmail.com>
2022-09-28 01:32:08 +0530

    Finished ../Q3_Sol.sql

---
+++ DML_Commands/Skill_Builder_Oracle/Q3_Sol.sql
@@ -0,0 +1,9 @@
+INSERT INTO Customers VALUES(301,'a','b','c','d','e','f','g','h');
+INSERT INTO Customers VALUES(302,'i','j','k','l','m','n','o','p');
+INSERT INTO Customers VALUES(303,'q','r','s','t','u','v','w','x');
+INSERT INTO Customers VALUES(304,'y','z','0','1','2','3','4','5');
+INSERT INTO Customers VALUES(305,'6','7','8','9','!','@','#','$');
+INSERT INTO Customers VALUES(306,'%','^','&','*','(',')','-','+');
+INSERT INTO Customers VALUES(307,'~','~','~','~','~','~','~','~');
+DELETE FROM Customers WHERE customer_id = 304;
+SELECT customer_id FROM Customers;
