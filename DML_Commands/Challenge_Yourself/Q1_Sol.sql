6a5a37e04335fed645e68317254942b6cbc24482
iamwatchdogs <shamith301102@gmail.com>
2022-09-28 01:50:11 +0530

    Finished ../Q1_Sol.sql

---
+++ DML_Commands/Challenge_Yourself/Q1_Sol.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (employee_id) VALUES(1);
+INSERT INTO employees (employee_id) VALUES(2);
+INSERT INTO employees (employee_id) VALUES(3);
+INSERT INTO employees (employee_id) VALUES(4);
+SELECT COUNT(*) FROM employees;
