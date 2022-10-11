6b858574e4c9828f2247421580f6aa003f40e1ce
Learn-git-SAM <simpilfy.git@gmail.com>
2022-10-12 01:40:25 +0530

    Solved Q3_Sol.sql

---
+++ Conversion Functions/Skill_Builder_Oracle/Q3_Sol.sql
@@ -0,0 +1,5 @@
+SELECT NAME,SUBSTR(NAME,LENGTH(NAME)) "LASTCHARACTER",SALARY FROM Employee;
+
+----------------------------------- OR ------------------------------------
+
+-- SELECT NAME,SUBSTR(NAME,-1) "LASTCHARACTER",SALARY FROM Employee;
