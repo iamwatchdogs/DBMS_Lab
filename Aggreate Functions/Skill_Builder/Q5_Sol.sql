0997aacd87a56d0b5165fc251759fa18d517ce1b
Shamith <shamith301102@gmail.com>
2022-11-02 10:36:01 +0530

    Update Q5_Sol.sql

--- Aggreate Functions/Skill_Builder/Q5_Sol.sql
+++ Aggreate Functions/Skill_Builder/Q5_Sol.sql
@@ -1,9 +1,9 @@
 SELECT e.Empno, e.Ename, d.Dname, a.num AS NUMBER_OF_EMPLOYEES
 FROM Employee e, Department d,
-    SELECT Manager, Empno, COUNT(*) AS num
+    (SELECT Manager Empno, COUNT(*) AS num
     FROM Employee
     GROUP BY Manager
     HAVING COUNT(*) = ( SELECT MAX(COUNT(*))
                         FROM Employee
-                        GROUP BY Manager) a
-WHERE e.Empno = a.Empno AND e.Deptno = D.Deptno;
+                        GROUP BY Manager)) AS a
+WHERE e.Empno = a.Empno AND e.Deptno = d.Deptno;
