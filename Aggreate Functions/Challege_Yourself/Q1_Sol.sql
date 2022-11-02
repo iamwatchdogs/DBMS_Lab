f8f24cb922ee6a98d66db11cf0d3f610af6fc6fd
Shamith <shamith301102@gmail.com>
2022-11-02 09:48:56 +0530

    Update Q1_Sol.sql

--- Aggreate Functions/Challege_Yourself/Q1_Sol.sql
+++ Aggreate Functions/Challege_Yourself/Q1_Sol.sql
@@ -1,1 +1,14 @@
+SELECT AVG(Salary)"AVERAGE",Deptno
+FROM Employee 
+WHERE Deptno = 1001
+GROUP BY Deptno;
 
+SELECT AVG(Salary)"AVERAGE",Deptno
+FROM Employee 
+WHERE Deptno = 2001
+GROUP BY Deptno;
+
+SELECT AVG(Salary)"AVERAGE",Deptno
+FROM Employee 
+WHERE Deptno = 3001
+GROUP BY Deptno;
