4db9a26d3f0332074b599fd1ba541e1d721c2442
Shamith <shamith301102@gmail.com>
2022-11-02 09:58:00 +0530

    Update Q3_Sol.sql

--- Aggreate Functions/Challege_Yourself/Q3_Sol.sql
+++ Aggreate Functions/Challege_Yourself/Q3_Sol.sql
@@ -1,1 +1,14 @@
+SELECT COUNT(*) as "COUNTEMP", Deptno
+FROM Employee
+WHERE Deptno = 1001
+GROUP BY Deptno;
 
+SELECT COUNT(*) as "COUNTEMP", Deptno
+FROM Employee
+WHERE Deptno = 2001
+GROUP BY Deptno;
+
+SELECT COUNT(*) as "COUNTEMP", Deptno
+FROM Employee
+WHERE Deptno = 3001
+GROUP BY Deptno;
