47c70678db16dbcc6406c5756a2df717401ff963
Shamith <shamith301102@gmail.com>
2022-12-27 00:31:09 +0530

    Create Q3_Sol.sql

---
+++ Lab_Exam_Questions/Q3_Sol.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Faculty 
+(
+    FacultyID VARCHAR(30) PRIMARY KEY,
+    Faculty_name VARCHAR(30),
+    Specialization VARCHAR(30)
+);
+
+CREATE TABLE Courses 
+(
+    CourseID INT PRIMARY KEY,
+    Course_name VARCHAR(30),
+    Course_fee INT,
+    FacultyID VARCHAR(30) REFERENCES Faculty(FacultyID)
+);
+
+CREATE TABLE Students 
+(
+    StudID INT PRIMARY KEY,
+    stud_name VARCHAR(30),
+    stud_addres VARCHAR(30),
+    CourseID INT REFERENCES Courses(CourseID)
+);
+
+---------------------------------------------------------
