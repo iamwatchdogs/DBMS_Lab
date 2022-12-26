c698257f47e7ec123e3911b1630464146d88d5c8
Shamith <shamith301102@gmail.com>
2022-12-27 00:48:54 +0530

    Update Q3_Sol.sql

--- Lab_Exam_Questions/Q3_Sol.sql
+++ Lab_Exam_Questions/Q3_Sol.sql
@@ -21,8 +21,25 @@ CREATE TABLE Students
     CourseID INT REFERENCES Courses(CourseID)
 );
 
----------------------------------------------------------
+--------------------------------------------------------------------------
 
 ALTER TABLE Courses ADD Course_duration NUMBER(10);
 
----------------------------------------------------------
+--------------------------------------------------------------------------
+
+INSERT INTO Faculty VALUES ( '10', 'Sri Ram', 'networking' );
+INSERT INTO Faculty VALUES ( '20', 'Shyam', 'dbms' );
+INSERT INTO Faculty VALUES ( '30', 'Pavan kumar', 'system design' );
+INSERT INTO Faculty VALUES ( '40', 'Narashima', 'advance technologies' );
+
+INSERT INTO Courses VALUES ( 211, 'DBMS', 2500, '10', 5 );
+INSERT INTO Courses VALUES ( 319, 'DC', 900, '20', 3 );
+INSERT INTO Courses VALUES ( 475, 'OS', 1000, '30', 4 );
+INSERT INTO Courses VALUES ( 695, 'IOT', 5000, '40', 9 );
+
+INSERT INTO Students VALUES ( 1, 'Sam', 'Vizag', 211 );
+INSERT INTO Students VALUES ( 2, 'Ram', 'Mumbai', 475 );
+INSERT INTO Students VALUES ( 3, 'Hari', 'Goa', NULL );
+INSERT INTO Students VALUES ( 4, 'Vasu', 'marripalem', 475 );
+
+--------------------------------------------------------------------------
