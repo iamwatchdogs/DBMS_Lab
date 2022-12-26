730f3fad6171aac9037fd5bdcda07024e391f6a4
Shamith <shamith301102@gmail.com>
2022-12-27 01:16:07 +0530

    Update Q3_Sol.sql

--- Lab_Exam_Questions/Q3_Sol.sql
+++ Lab_Exam_Questions/Q3_Sol.sql
@@ -10,7 +10,8 @@ CREATE TABLE Courses
     CourseID INT PRIMARY KEY,
     Course_name VARCHAR(30),
     Course_fee INT,
-    FacultyID VARCHAR(30) REFERENCES Faculty(FacultyID)
+    FacultyID VARCHAR(30),
+    CONSTRAINT fk1 FOREIGN KEY (FacultyID) REFERENCES Faculty(FacultyID)
 );
 
 CREATE TABLE Students 
@@ -30,12 +31,12 @@ ALTER TABLE Courses ADD Course_duration NUMBER(10);
 INSERT INTO Faculty VALUES ( '10', 'Sri Ram', 'networking' );
 INSERT INTO Faculty VALUES ( '20', 'Shyam', 'dbms' );
 INSERT INTO Faculty VALUES ( '30', 'Pavan kumar', 'system design' );
-INSERT INTO Faculty VALUES ( '40', 'Narashima', 'advance technologies' );
+INSERT INTO Faculty VALUES ( '45', 'Narashima', 'advance technologies' );
 
 INSERT INTO Courses VALUES ( 211, 'DBMS', 2500, '10', 5 );
 INSERT INTO Courses VALUES ( 319, 'DC', 900, '20', 3 );
 INSERT INTO Courses VALUES ( 475, 'OS', 1000, '30', 4 );
-INSERT INTO Courses VALUES ( 695, 'IOT', 5000, '40', 9 );
+INSERT INTO Courses VALUES ( 695, 'IOT', 5000, '45', 9 );
 
 INSERT INTO Students VALUES ( 1, 'Sam', 'Vizag', 211 );
 INSERT INTO Students VALUES ( 2, 'Ram', 'Mumbai', 475 );
@@ -54,3 +55,7 @@ SELECT Faculty_name FROM Faculty WHERE Specialization = 'networking';
 
 -----------------------------------------------------------------------------------------------------------------
 
+ALTER TABLE Courses DROP CONSTRAINT fk1;
+UPDATE Faculty SET FacultyID = '40' WHERE FacultyID = '20';
+
+-----------------------------------------------------------------------------------------------------------------
