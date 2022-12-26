a00f0d3fadc2764383001a077c5d91d11742ef94
Shamith <shamith301102@gmail.com>
2022-12-26 23:28:53 +0530

    Update Q2_Sol.sql

--- Lab_Exam_Questions/Q2_Sol.sql
+++ Lab_Exam_Questions/Q2_Sol.sql
@@ -17,4 +17,16 @@ CREATE TABLE Books
     LibMemberid INT REFERENCES Library(LibMemberID)
 );
 
--------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO Library VALUES ( 783, 'Vasu', 'CSE', 2, 500 );
+INSERT INTO Library VALUES ( 785, 'Shiva', 'EEE', 1, 750 );
+INSERT INTO Library VALUES ( 787, 'Priya', 'MEC', 4, 350 );
+INSERT INTO Library VALUES ( 789, 'Kumar', 'ECE', 3, 100 );
+
+INSERT INTO Books VALUES ( 121, 'Database_system_concepts', 'Shashi ranjan', 'Chota rajan publications', 1000, 783  );
+INSERT INTO Books VALUES ( 133, 'Romeo and Juliet', 'Shakespeare', 'Harcourt Canada', 800, 785 );
+INSERT INTO Books VALUES ( 275, 'Tata Motors car design', 'Tata industries', 'KR publications', 1900, 787 );
+INSERT INTO Books VALUES ( 369, 'Microelectronics', 'R.K.Wilson', 'New Foundation publications', 1500, 789 );
+
+-----------------------------------------------------------------------------------------------------------------------
